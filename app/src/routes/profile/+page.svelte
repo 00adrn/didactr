9bba60e24c09932ce6289c761a86983e0897207a
00adrn <adrianvez11@gmail.com>
2026-03-08 05:04:28 -0400

    Placeholder for profile page.

--- app/src/routes/profile/+page.svelte
+++ app/src/routes/profile/+page.svelte
@@ -2,8 +2,11 @@
 
 </script>
 
-<div class="flex justify-center items-center h-full w-full">
-    <p class="text-8xl">
-        Profiles, rankings, friends, and more coming soon! (I am a one man team)
+<div class="flex justify-center items-center h-full w-full flex-col">
+    <p class="text-6xl text-stone-50 font-bold text-center">
+        Profiles, rankings, friends, and more coming soon!
+    </p>
+    <p class="text-6xl text-stone-50 font-bold text-center">
+    (I am a one man team)
     </p>
 </div>
