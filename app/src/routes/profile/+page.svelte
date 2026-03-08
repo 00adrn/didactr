6d3271163a60e3a9225a0a8de400629eb7edb967
00adrn <adrianvez11@gmail.com>
2026-03-08 05:02:38 -0400

    Profile page

---
+++ app/src/routes/profile/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<div class="flex justify-center items-center h-full w-full">
+    <p class="text-8xl">
+        Profiles, rankings, friends, and more coming soon! (I am a one man team)
+    </p>
+</div>
